66a29d214a9a47e8c33b9cbd5876f7c2b0445e59
teddmhndr <123808974+teddmhndr@users.noreply.github.com>
2023-02-12 10:24:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 @app.route('/',methods=['GET'])
 
 def home():
-    return"<h1> it works </h1>"
+    return"<h1> Mahen </h1>"
 
 if __name__=='__main__':
     app.run(debug=True, host="0.0.0.0")
